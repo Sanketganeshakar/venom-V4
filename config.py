64461667ed07cbccd4846ec21eac933c62cbc9b1
Sanketganeshakar <141744789+Sanketganeshakar@users.noreply.github.com>
2024-07-28 21:42:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7076471682:AAEn8dydgBvrs0sJApMR8ZNxZCDpRLSCMXk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("NNx venom")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("-4224584248")
 
 
 
