b35c072eb1aa142d1218711a6d3d6157943c7eff
Sanketganeshakar <141744789+Sanketganeshakar@users.noreply.github.com>
2024-07-28 22:42:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7076471682:AAEn8dydgBvrs0sJApMR8ZNxZCDpRLSCMXk")
+BOT_TOKEN=("7076471682:AAHytKT0sfvsuYHLN4PC_noeSCSJUIVpYlA")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("NNx venom")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("-4224584248")
+ADMIN_IDS=("1781947522")
 
 
 
